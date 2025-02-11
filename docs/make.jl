5a59ab5fbcfc7680f6a118efd1f0e9ccf55bdff9
Rajmadan Lakshmanan <62553056+rajmadan96@users.noreply.github.com>
2025-02-11 14:10:24 +0100

    Add files via upload

---
+++ docs/make.jl
@@ -0,0 +1,26 @@
+using StochasticDominance
+using Documenter
+#push!(LOAD_PATH,"/home/mathe/lvhome10/users/personal/lraj/Nextcloud/SD/HOME1/users/personal/lraj/Nextcloud/SD/Github/StochasticDominance/Project.toml")
+#DocMeta.setdocmeta!(StochasticDominance, :DocTestSetup, :(using StochasticDominance); recursive=true)
+
+makedocs(
+	sitename =  "StochasticDominance.jl",
+	authors = "Rajmadan Lakshmanan",
+	clean = true,
+	doctest = false,
+	format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
+	pages = ["Home" => "index.md",
+		"Tutorials" => Any["tutorial/tutorial1.md",
+				    "tutorial/tutorial2.md",
+				    ]
+		]
+)
+
+deploydocs(; 
+	repo = "https://github.com/rajmadan96/StochasticDominance.jl.git",
+	target = "build",
+	devbranch = "master",
+    	versions = ["stable" => "v^", "v#.#"]
+)
+
+
