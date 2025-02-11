0fb3fcb99f0020bd52aa69eee8c063729a10df3c
Rajmadan Lakshmanan <62553056+rajmadan96@users.noreply.github.com>
2025-02-11 10:33:18 +0100

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,25 +1,26 @@
 using StochasticDominance
 using Documenter
-push!(LOAD_PATH,"/home/mathe/lvhome10/users/personal/lraj/Nextcloud/SD/HOME1/users/personal/lraj/Nextcloud/SD/Github/StochasticDominance/Project.toml")
+#push!(LOAD_PATH,"/home/mathe/lvhome10/users/personal/lraj/Nextcloud/SD/HOME1/users/personal/lraj/Nextcloud/SD/Github/StochasticDominance/Project.toml")
 #DocMeta.setdocmeta!(StochasticDominance, :DocTestSetup, :(using StochasticDominance); recursive=true)
 
-makedocs(;
-    modules=[StochasticDominance],
-    authors="Rajmadan Lakshmanan",
-    sitename="StochasticDominance.jl",
-    # format=Documenter.HTML(;
-    #     canonical="https://rajmadan96.github.io/StochasticDominance.jl",
-    #     edit_link="main",
-    #     assets=String[],
-    # ),
-    pages=[
-        "Home" => "index.md",
-    ],
+makedocs(
+	sitename =  "StochasticDominance.jl",
+	authors = "Rajmadan Lakshmanan",
+	clean = true,
+	doctest = false,
+	format = Documenter.HTML(;canonical="https://rajmadan96.github.io/StochasticDominance.jl"),
+	pages = ["Home" => "index.md",
+		"Tutorials" => Any["tutorial/tutorial1.md",
+				    "tutorial/tutorial2.md",
+				    ]
+		]
 )
 
-deploydocs(;
-    repo="github.com/rajmadan96/StochasticDominance.jl",
-    devbranch="main"
+deploydocs(; 
+	repo = "https://github.com/rajmadan96/StochasticDominance.jl.git",
+	target = "build",
+	devbranch = "master",
+    	versions = ["stable" => "v^", "v#.#"]
 )
 
 
